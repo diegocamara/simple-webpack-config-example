0cbde7a8bc51a4a295de590ad95a40311f6057ab
Diego Câmara <diegoluizjava.java@gmail.com>
2019-07-09 20:37:22 -0300

    Configuração inicial

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/js/app.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "src/app_bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/html/index.html",
+            filename: "index.html",
+            hash: true
+        })
+    ]
+
+};
